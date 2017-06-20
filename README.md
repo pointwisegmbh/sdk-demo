670ebdcf458dfdffae736f2a98399c0411c04129
Andrey Zaytsev <a.einsam@gmail.com>
2017-06-20 11:33:48 +0200

    Update SDK to 1.5.0

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Documentation
 
 You can check the documentation [here](https://docs.locarta.co/).
+
+# Build
+
+To build this project you have to create a file `credentials.properties` in a root folder of this project and fill it according to `credentials.properties.example`.
+It contains credentials to download the Locarta SDK dependency and your publisher id.
--- app/src/main/java/co/locarta/sdk/demo/MainActivity.java
+++ app/src/main/java/co/locarta/sdk/demo/MainActivity.java
@@ -11,9 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import co.locarta.sdk.LocartaSdk;
-import co.locarta.sdk.common.TermsStatus;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION};
     private static final int REQUEST_PERMISSION = 0;
 
     @Override
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * After getting a Manifest.permission.ACCESS_FINE_LOCATION for Android >= 23 you can display this
-     * built-in dialog or simple call {@link LocartaSdk#setAgreementAccepted(Context, boolean)}. If you
+     * built-in dialog or just call {@link LocartaSdk#setAgreementAccepted(Context, boolean)}. If you
      * want you can call those methods in right after user started the app. Locarta SDK supports permissions
      * and do not collect the data we don't have permission for.
      * <p/>
@@ -39,21 +39,31 @@ public class MainActivity extends AppCompatActivity {
      */
     private void showAgreementDialog() {
         if (!LocartaSdk.isAgreementAccepted(this)) {
-            LocartaSdk.setAgreementAccepted(this, true);
+            LocartaSdk.showAgreementDialog(this);
         }
     }
 
     private void askForPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-            Toast.makeText(this, "We need a location permission to track your location for marketing research", Toast.LENGTH_SHORT).show();
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION);
+            explainPermissionAndRequest();
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION);
+            doRequestPermission();
         }
     }
 
+    private void explainPermissionAndRequest() {
+        Toast.makeText(this, R.string.permission_explaination, Toast.LENGTH_SHORT).show();
+        doRequestPermission();
+    }
+
+    private void doRequestPermission() {
+        ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSION);
+    }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_PERMISSION:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
